6f5adadb94b6342e54094411fc9ced5d659ce755
fanyingfx <fanzhenxin@outlook.com>
2022-07-18 17:17:56 +0800

    init

---
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,13 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+public class Scanner {
+    public Scanner(String source) {
+
+    }
+
+    public List<Token> scanTokens() {
+        return null;
+    }
+}
